0c91c347d5062605509e52ee1f0ec1130eed70a4
dsgpt <130886885+dsgpt@users.noreply.github.com>
2023-11-20 21:33:34 -0500

    remove api key

--- Config.py
+++ Config.py
@@ -1,7 +1,7 @@
 class Config:
     def __init__(self):
         # OpenAI API Key
-        self.OPENAI_API_KEY = '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'
+        self.OPENAI_API_KEY = ''
 
     def get_openai_api_key(self):
         return self.OPENAI_API_KEY
